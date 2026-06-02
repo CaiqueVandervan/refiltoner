bfaf6609f290fe61566c048342e534c41a72c6a9
Caique Vandervan <CAIQUEVJA481@GMAIL.COM>
2026-06-02 00:56:05 -0300

    feat: add Brands component

---
+++ src/components/Brands.tsx
@@ -0,0 +1,9 @@
+const Brands = () => {
+    return(
+        <section className="flex items-center justify-center p-12">
+            <h1>Marcas</h1>
+        </section>
+    )
+}
+
+export default Brands
