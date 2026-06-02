008e9c6cad4643948f6689572a451fde9a52088d
Caique Vandervan <CAIQUEVJA481@GMAIL.COM>
2026-06-02 00:57:45 -0300

    feat: assembled page with all sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
 import WhatsappButton from "@/components/WhatsappButton"
 import NavBar from "@/components/NavBar"
+import HeroSection from "@/components/HeroSection"
+import HowWeWork from "@/components/HowWeWork"
+import WhoWeServe from "@/components/WhoWeServe"
+import Brands from "@/components/Brands"
+import Contact from "@/components/Contact"
 
 const Refiltoner = () => {
-  
-  return(
+
+  return (
     <main>
-      <NavBar/>
-      <WhatsappButton/>
+      <NavBar />
+
+      <HeroSection />
+
+      <HowWeWork />
+
+      <WhoWeServe/>
+
+      <Brands/>
+
+      <Contact/>
+
+      <WhatsappButton />
     </main>
   )
 }
