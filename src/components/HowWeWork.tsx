9d2c2031eaebcf39837ff8c32bab215c69c9a349
Caique Vandervan <CAIQUEVJA481@GMAIL.COM>
2026-06-02 00:43:32 -0300

    feat: add HowWeWork component

---
+++ src/components/HowWeWork.tsx
@@ -0,0 +1,9 @@
+const HowWeWork = () => {
+    return (
+        <section className="flex items-center justify-center p-12">
+            <h1>Como Trabalhamos</h1>
+        </section>
+    )
+}
+
+export default HowWeWork
