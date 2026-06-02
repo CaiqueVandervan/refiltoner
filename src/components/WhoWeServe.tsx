60916c8f4d1e159377c3fcf42b916000af9d00b2
Caique Vandervan <CAIQUEVJA481@GMAIL.COM>
2026-06-02 00:55:42 -0300

    feat: add WhoWeServe component

---
+++ src/components/WhoWeServe.tsx
@@ -0,0 +1,9 @@
+const WhoWeServe = () => {
+    return (
+        <section className="flex items-center justify-center p-12 bg-gray-200">
+            <h1>Quem Atendemos</h1>
+        </section>
+    )
+}
+
+export default WhoWeServe
