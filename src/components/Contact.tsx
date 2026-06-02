042becf5b892d1c44fcc4ebc372f13eabf65bac0
Caique Vandervan <CAIQUEVJA481@GMAIL.COM>
2026-06-02 00:56:31 -0300

    feat: add Contact component

---
+++ src/components/Contact.tsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+    return (
+        <section className="flex items-center justify-center p-12 bg-[#8B1E23]">
+            <h1>Contato</h1>
+        </section>
+    )
+}
+
+export default Contact
