6d308eb447b192601a233fddda8a774bb1fac7f9
Caique481 <caique.araujo@gmaster.com.br>
2026-07-28 16:14:09 -0300

    feat: new section for the site

---
+++ src/components/sections/About.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <section id="sobre" className="flex items-center justify-center p-12">
+            <h1>Sobre</h1>
+        </section>
+    )
+}
+
