3d2f6abbd4929f3f399a43d4e6d30f6e349318b2
Caique481 <caique.araujo@gmaster.com.br>
2026-07-28 16:13:34 -0300

    style: change site font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Open_Sans } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +11,10 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const openSans = Open_Sans({
+  subsets: ["latin"]
+})
 
 export const metadata: Metadata = {
   title: "Refiltoner",
@@ -27,7 +31,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className={`${geistSans.className} min-h-full flex flex-col`}>{children}</body>
+      <body className={`${openSans.className} min-h-full flex flex-col`}>{children}</body>
     </html>
   );
 }
