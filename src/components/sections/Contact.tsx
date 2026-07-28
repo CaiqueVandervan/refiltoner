ff2293423b43b6504cde5b59acc55b437e81e7d3
Caique481 <caique.araujo@gmaster.com.br>
2026-07-28 16:16:12 -0300

    refactor: improve word spacing and reorganize file structure

---
+++ src/components/sections/Contact.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+        <section id="contato" className="flex items-center justify-center p-12 bg-[#8B1E23]">
+            <h1>Contato</h1>
+        </section>
+    )
+}
+
---
+++ src/components/sections/WhoWeServe.tsx
@@ -0,0 +1,7 @@
+export default function WhoWeServe() {
+    return (
+        <section id="quematendemos" className="flex items-center justify-center p-12 bg-gray-200">
+            <h1>Quem Atendemos</h1>
+        </section>
+    )
+}
