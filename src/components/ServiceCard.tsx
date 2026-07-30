3d171c43b79f75edfa5e989afd5596d0919e1f50
Caique481 <caique.araujo@gmaster.com.br>
2026-07-30 18:16:59 -0300

    feat: add mock content and background image

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -3,6 +3,7 @@ import { Icon } from "@iconify/react";
 interface ServiceCard {
     id: number
     img: string
+    icon: string
     title: string
     subTitle: string
     checkTexts: string[]
@@ -14,24 +15,32 @@ interface ServiceCardProps {
 
 export default function ServiceCard({ content }: ServiceCardProps) {
     return (
-        <div className="flex gap-2">
+        <div className="flex gap-4 z-2">
             {content.map((c) => (
-                <div key={c.id} className="bg-gray-100 rounded-xl p-4 w-md">
-                    <div className="h-20">
-                        <img src={c.img} />
+                <div key={c.id} className="relative bg-gray-100 rounded-xl w-md shadow-xl hover:scale-103 duration-300">
+                    <div >
+                        <img src={c.img} className="rounded-t-xl" />
                     </div>
 
-                    <div className="flex flex-col gap-4">
-                        <h1 className="text-center font-bold text-[#33363B] text-[20px]">{c.title}</h1>
-                        <h2 className="px-2">{c.subTitle}</h2>
+                    <div className="absolute top-[43%] left-[45%] bg-[#8A8F96] rounded-full p-5">
+                        <Icon icon={c.icon} color="white" fontSize={25} />
+                    </div>
 
+                    <div className="flex flex-col items-center gap-4 p-4 pt-10">
+                        <h1 className="font-bold text-[#33363B] text-[20px]">{c.title}</h1>
+                        <span className="h-0.5 bg-[#8B1E23] flex items-center justify-center w-[50%] rounded-md" />
+                        <h2 className="px-2">{c.subTitle}</h2>
+                    </div>
+
+                    <div className="px-6 pb-4">
                         {c.checkTexts.map((t) => (
-                            <div key={t} className="flex items-center">
+                            <div key={t} className="flex items-center gap-1">
                                 <Icon icon="lets-icons:check-fill" color="#8B1E23" />
                                 {t}
                             </div>
                         ))}
                     </div>
+
                 </div>
             ))}
         </div>
