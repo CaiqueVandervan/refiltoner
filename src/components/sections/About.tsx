7df2a652cfd18f096c3ea17f84d5161838c31eff
Caique481 <caique.araujo@gmaster.com.br>
2026-07-30 18:15:33 -0300

    feat: add mock content for About section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,7 +1,32 @@
+import Contact from "./Contact";
+
 export default function About() {
     return (
-        <section id="sobre" className="flex items-center justify-center p-12">
-            <h1>Sobre</h1>
+        <section id="sobre" className="relative bg-[url('/fundoAbout4.png')] bg-cover bg-center px-22 p-8">
+
+            <h1 className="text-[#33363B] font-bold text-[20px] text-center">Sobre nossa empresa</h1>
+
+            <div className="flex gap-10 pt-8 px-12">
+                <div className="w-1/2">
+                    <img src="fundoHero5.png" className="rounded-xl" />
+                </div>
+
+                <div className="w-1/2 flex flex-col gap-3 pr-50 font-medium">
+                    <p>
+                        Há anos, ajudamos empresas a manterem seus negócios em movimento.
+                        A [Nome da Empresa] nasceu com o propósito de oferecer soluções completas em impressão, unindo qualidade, agilidade e atendimento próximo. Atuamos com venda, locação e manutenção de impressoras e plotters, além de uma linha completa de suprimentos e acessórios para atender às diferentes necessidades de cada cliente.
+                    </p>
+                    <p>
+                        Ao longo da nossa trajetória, construímos nossa experiência com base em um compromisso simples: entender as necessidades de cada empresa e oferecer a solução mais adequada para garantir produtividade e tranquilidade no dia a dia.
+                    </p>
+                    <p>
+                        Mais do que fornecer equipamentos, buscamos construir relações de confiança e parcerias duradouras, oferecendo suporte especializado e soluções que acompanham o crescimento de nossos clientes.
+                    </p>
+                </div>
+
+            </div>
+
+            <Contact />
         </section>
     )
 }
